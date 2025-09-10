c69c934a1bd6ade4bdb26d3549299ec09111b9a3
Sukh Singh <sukhsinghntn@gmail.com>
2025-09-10 07:23:15 -0400

    test

--- Client/obj/Release/net8.0/NDAProcesses.Client.AssemblyInfo.cs
+++ Client/obj/Release/net8.0/NDAProcesses.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NDAProcesses.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f727d3c759976b50e60d37f2d13529541dbe848")]
 [assembly: System.Reflection.AssemblyProductAttribute("NDAProcesses.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NDAProcesses.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Server/obj/Release/net8.0/NDAProcesses.Server.AssemblyInfo.cs
+++ Server/obj/Release/net8.0/NDAProcesses.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NDAProcesses.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f727d3c759976b50e60d37f2d13529541dbe848")]
 [assembly: System.Reflection.AssemblyProductAttribute("NDAProcesses.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NDAProcesses.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
